441c10db2f3877d9025c60995ea046b32751d56f
Andrew Hopkins <anhopki@microsoft.com>
2023-06-30 16:39:46 -0400

    project init

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace csharp_sql
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
