4af5dc42d9b867c49fbecd14efae6c7c46cc98f4
ahopkins <ahopkins0929@gmail.com>
2023-08-06 15:05:01 -0400

    fix unit tests

--- csharp_sql.Tests/csharp_sql.Tests/LexerTests.cs
+++ csharp_sql.Tests/csharp_sql.Tests/LexerTests.cs
@@ -48,7 +48,7 @@ namespace csharp_sql.Tests
         [TestCase("'*dje*$%@#$'")]
         public void LexTest_TestLexingString_ReturnsExpectedTokenWithStringType(string source)
         {
-            var expectedToken = new Token { Location = new Location { Column = 0, Row = 0 }, TokenType = TokenType.String, Value = $"{source}" };
+            var expectedToken = new Token { Location = new Location { Column = 0, Row = 0 }, TokenType = TokenType.String, Value = $"{source}".Substring(1, source.Count() - 2) };;
 
             var lexer = new Lexer(source);
             var tokens = lexer.Lex();
@@ -121,8 +121,8 @@ namespace csharp_sql.Tests
                 "'test' 'test2'",
                 new List<Token>
                 {
-                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "'test'" },
-                    new Token { Location = new Location{ Column = 7, Row = 0 }, TokenType = TokenType.String, Value = "'test2'" },
+                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "test" },
+                    new Token { Location = new Location{ Column = 7, Row = 0 }, TokenType = TokenType.String, Value = "test2" },
                 }
             },
             new object[] 
@@ -130,10 +130,10 @@ namespace csharp_sql.Tests
                 "'test tes testing'  'more testing'  'evenmore testing'  '' ",
                 new List<Token>
                 { 
-                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "'test tes testing'" },
-                    new Token { Location = new Location{ Column = 20, Row = 0 }, TokenType = TokenType.String, Value = "'more testing'" },
-                    new Token { Location = new Location{ Column = 36, Row = 0 }, TokenType = TokenType.String, Value = "'evenmore testing'" },
-                    new Token { Location = new Location{ Column = 56, Row = 0 }, TokenType = TokenType.String, Value = "''" },
+                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "test tes testing" },
+                    new Token { Location = new Location{ Column = 20, Row = 0 }, TokenType = TokenType.String, Value = "more testing" },
+                    new Token { Location = new Location{ Column = 36, Row = 0 }, TokenType = TokenType.String, Value = "evenmore testing" },
+                    new Token { Location = new Location{ Column = 56, Row = 0 }, TokenType = TokenType.String, Value = "" },
                 }
             },
             new object[] 
@@ -141,8 +141,8 @@ namespace csharp_sql.Tests
                 "'testing'\n'testing2'",
                 new List<Token>
                 { 
-                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "'testing'" },
-                    new Token { Location = new Location{ Column = 0, Row = 1 }, TokenType = TokenType.String, Value = "'testing2'" },
+                    new Token { Location = new Location{ Column = 0, Row = 0 }, TokenType = TokenType.String, Value = "testing" },
+                    new Token { Location = new Location{ Column = 0, Row = 1 }, TokenType = TokenType.String, Value = "testing2" },
                 }
             },
             new object[] 
@@ -174,7 +174,7 @@ namespace csharp_sql.Tests
                     new Token { Location = new Location { Column = 45, Row = 0}, TokenType = TokenType.RightParen, Value = ")" },
                     new Token { Location = new Location { Column = 47, Row = 0}, TokenType = TokenType.Values, Value = "VALUES" },
                     new Token { Location = new Location { Column = 54, Row = 0}, TokenType = TokenType.LeftParen, Value = "(" },
-                    new Token { Location = new Location { Column = 55, Row = 0}, TokenType = TokenType.String, Value = "'Q42023'" },
+                    new Token { Location = new Location { Column = 55, Row = 0}, TokenType = TokenType.String, Value = "Q42023" },
                     new Token { Location = new Location { Column = 63, Row = 0}, TokenType = TokenType.Comma, Value = "," },
                     new Token { Location = new Location { Column = 65, Row = 0}, TokenType = TokenType.Numeric, Value = "13241" },
                     new Token { Location = new Location { Column = 70, Row = 0}, TokenType = TokenType.Comma, Value = "," },
